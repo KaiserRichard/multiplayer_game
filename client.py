58f9ff3ed4bdaa23c46c43c1e3dc3a92f4a68ef9
KaiserQuoc <kaiserquoc@gmail.com>
2026-01-30 21:53:14 +0700

    Step 3: client-server connection and game configuration handshake

--- client.py
+++ client.py
@@ -1,115 +1,108 @@
-#Online multiplayer game client 
+# Online multiplayer game client
 import pygame, socket, threading, json
 
-#Define socket constatns to be used and altered
-#DEST_IP should be of the form '192.168.1.*'
 DEST_IP = socket.gethostbyname(socket.gethostname())
 DEST_PORT = 12345
 
-class Connection():
-    '''A socket connection class for players to connect to a server'''
 
+class Connection:
     def __init__(self):
-        '''Initialization for the Connection class'''
-        pass
+        self.encoder = "utf-8"
+        self.header_length = 10
+        self.player_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.player_socket.connect((DEST_IP, DEST_PORT))
 
 
-class Player():
-    '''A player class the client can control'''
-    def __init__(self, connetion):
-        '''Initializetion of the Player class'''
-        pass
+class Player:
+    def __init__(self, connection):
+        self.connection = connection
 
     def set_player_info(self, player_info):
-        '''Set the player info to the given information from the server'''
         pass
 
     def update(self):
-        '''Update the player by changing their coordinates in the game'''
+        pass
 
     def reset_player(self):
-        ''''Reset player values for a new round on the client side'''
         pass
 
-class Game():
-    '''A game clas to handle all operations of gameplay'''
+
+class Game:
     def __init__(self, connection, player, total_players):
-        pass
-
-    def ready_game(self):
-        pass
-
-    def start_game(self):
-        pass
-
-    def reset_game(self):
-        pass
-
-    def send_player_info(self):
-        '''Send specific info about this player to the server'''
-        pass
-
-    def receive_player_info(self):
-        '''Receive player info from the server'''
-        pass
-
-    def receive_pregame_state(self):
-        '''Receive all ìnormation about all players from the server during the game'''
-        pass
-
-    def process_game_state(self):
-        '''Process the game state to update scores '''
-        pass
+        self.connection = connection
+        self.player = player
+        self.total_players = total_players
 
     def update(self):
-        '''Update the game'''
         pass
 
     def draw(self):
-        '''Draw the game and all game assets to the window'''
         pass
 
-#Create a connection and get wwindow information from the server
 
+# ---- RECEIVE GAME CONFIG FROM SERVER (TUTORIAL STYLE) ----
 my_connection = Connection()
 
-#initialize pygame
+packet_size = my_connection.player_socket.recv(
+    my_connection.header_length
+).decode(my_connection.encoder)
+room_size = int(
+    my_connection.player_socket.recv(int(packet_size))
+    .decode(my_connection.encoder)
+)
+
+packet_size = my_connection.player_socket.recv(
+    my_connection.header_length
+).decode(my_connection.encoder)
+round_time = int(
+    my_connection.player_socket.recv(int(packet_size))
+    .decode(my_connection.encoder)
+)
+
+packet_size = my_connection.player_socket.recv(
+    my_connection.header_length
+).decode(my_connection.encoder)
+fps = int(
+    my_connection.player_socket.recv(int(packet_size))
+    .decode(my_connection.encoder)
+)
+
+packet_size = my_connection.player_socket.recv(
+    my_connection.header_length
+).decode(my_connection.encoder)
+total_players = int(
+    my_connection.player_socket.recv(int(packet_size))
+    .decode(my_connection.encoder)
+)
+
+# ---- PYGAME SETUP ----
 pygame.init()
-WINDOW_WIDTH = 700
-WINDOW_HEIGHT = 700
-ROUND_TIME = 60
+
+WINDOW_WIDTH = room_size
+WINDOW_HEIGHT = room_size
 BLACK = (0, 0, 0)
-WHITE = (255, 255, 255)
-MAGENTA = (155, 0, 155)
-FPS = 30
+FPS = fps
 clock = pygame.time.Clock()
 
-font = pygame.font.SysFont('gabriola', 28)
-
-#Create a game window
 display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption("~~Color Collide~~")
 
-#Create player and game objects
 my_player = Player(my_connection)
-my_game = Game(my_connection, my_player, 4)
+my_game = Game(my_connection, my_player, total_players)
 
-#The main game loop 
+# ---- MAIN LOOP ----
 running = True
 while running:
-    #Check to see if the user wants to quit
-    for even in pygame.event.get():
-        if even.type == pygame.QUIT:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
             running = False
 
-    #Fill the surface 
     display_surface.fill(BLACK)
-
-    #Update and draw classes
     my_player.update()
     my_game.update()
     my_game.draw()
 
-    #Update the display and tick the clock
     pygame.display.update()
     clock.tick(FPS)
+
+pygame.quit()
--- server.py
+++ server.py
@@ -1,84 +1,82 @@
-#Online multiplayer game client 
+# Online multiplayer game server
 import socket, threading, json, time
-#Network configuration
 
 HOST_IP = socket.gethostbyname(socket.gethostname())
 HOST_PORT = 12345
 
-#Game configuration
 ROOM_SIZE = 400
 PLAYER_SIZE = 20
 ROUND_TIME = 45
 FPS = 15
-TOTAL_PLAYERS = 4                   
+TOTAL_PLAYERS = 4
 
-#Ensure room and player sizes align
 while ROOM_SIZE % PLAYER_SIZE != 0:
     PLAYER_SIZE += 1
 
-class Connection():
+
+class Connection:
     def __init__(self):
-        pass
+        self.encoder = "utf-8"
+        self.header_length = 10
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server_socket.bind((HOST_IP, HOST_PORT))
+        self.server_socket.listen()
 
-class Player():
+
+class Player:
     def __init__(self, player_id):
-        self.player_id  = player_id
-    
-    def set_player_info(self, info):
-        pass
+        self.player_id = player_id
 
-    def reset_player(self):
-        '''Set the player info to given info from the client'''
-        pass
 
-    def reset_player(self):
-        '''Reset player values for a new round on the server side'''
-        pass
-
-class Game():
-    '''A class to handle all the operations of gameplay'''
+class Game:
     def __init__(self, connection):
-        '''Initialzation of the Game class'''
+        self.connection = connection
+        self.player_count = 0
+        self.player_objects = []
+        self.player_sockets = []
 
     def connect_players(self):
-        '''Conect any incoming players to the game server'''
-        pass
+        while self.player_count < TOTAL_PLAYERS:
+            player_socket, player_address = self.connection.server_socket.accept()
 
-    def broadcast(self):
-        '''Broadcast information to all connected players'''
-        pass
+            # SEND ROOM_SIZE
+            header = str(len(str(ROOM_SIZE)))
+            header = header.ljust(self.connection.header_length)
+            player_socket.send(header.encode(self.connection.encoder))
+            player_socket.send(str(ROOM_SIZE).encode(self.connection.encoder))
 
-    def ready_game(self, player, player_socket):
-        '''Ready the game to be played'''
-        pass
+            # SEND ROUND_TIME
+            header = str(len(str(ROUND_TIME))).ljust(self.connection.header_length)
+            player_socket.send(header.encode(self.connection.encoder))
+            player_socket.send(str(ROUND_TIME).encode(self.connection.encoder))
 
-    def reset_game(self, player):
-        '''Restart the game and wipe information for a specific player'''
-        pass
+            # SEND FPS
+            header = str(len(str(FPS))).ljust(self.connection.header_length)
+            player_socket.send(header.encode(self.connection.encoder))
+            player_socket.send(str(FPS).encode(self.connection.encoder))
 
-    def send_player_infor(self, player, player_socket):
-        '''Send specific information about this player to the given client'''
-        pass
+            # SEND TOTAL_PLAYERS
+            header = str(len(str(TOTAL_PLAYERS))).ljust(self.connection.header_length)
+            player_socket.send(header.encode(self.connection.encoder))
+            player_socket.send(str(TOTAL_PLAYERS).encode(self.connection.encoder))
 
-    def receive_pregrame_player_info(self, player, player_socket):
-        '''Receive specific info about this player pregame'''
-        pass
+            self.player_count += 1
+            player = Player(self.player_count)
+            self.player_objects.append(player)
+            self.player_sockets.append(player_socket)
 
-    def receive_game_player_info(self, play):
-        '''Receive spefici info about this player during the game'''
-        pass
+            print(f"Player {self.player_count} connected from {player_address}")
 
-    def process_game_state(self, player, player_socket):
-        '''Process the given player info and update the games state'''
-        pass
+        print("All players connected. Server running...")
 
-    def send_game_state(self, player_socket):
-        '''Send the current game state of all players to this given player'''
-        pass
+        # KEEP SERVER ALIVE (tutorial assumption)
+        while True:
+            time.sleep(1)
 
-#Start the server
+
+# ---- START SERVER ----
 my_connection = Connection()
 my_game = Game(my_connection)
-#Server is listening to incoming connections
-print("Server is listening to incoming connections....")
+
+print("Server is listening for incoming connections...")
 my_game.connect_players()
